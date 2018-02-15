097c59bf22c8a6dff3b4f1de0df70a289281f043
uanoob <usnet@ukr.net>
2018-02-15 12:02:02 +0200

    Fix bug, use ternary

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,3 @@
 // keys.js - figure out what set of credentials to return
-const prod = require('./prod');
-const dev = require('./dev');
 
-if (process.env.NODE_ENV === 'production') {
-  // we are in production - return the prod set of keys
-  module.exports = prod;
-} else {
-  // we are in development - return the dev keys!!!
-  module.exports = dev;
-}
+module.exports = process.env.NODE_ENV === 'production' ? require('./prod') : require('./dev');
