d590ac0c851b09c2f94c5eda6f1c550a89200fa7
uanoob <usnet@ukr.net>
2018-02-16 09:55:37 +0200

    Change structure file

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,7 @@
 // keys.js - figure out what set of credentials to return
 
-module.exports = process.env.NODE_ENV === 'production' ? require('./prod') : require('./dev');
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod');
+} else {
+  module.exports = require('./dev');
+}
