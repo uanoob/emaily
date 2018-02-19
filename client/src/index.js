533e2efe01b4751e3d02758463a296c65e856ab6
uanoob <usnet@ukr.net>
2018-02-19 16:47:06 +0200

    Bit change

--- client/src/index.js
+++ client/src/index.js
@@ -16,6 +16,3 @@ ReactDom.render(
   </Provider>,
   document.getElementById('root'),
 );
-
-console.log('STRIPE KEY IS', process.env.REACT_APP_STRIPE_KEY);
-console.log('Environment is', process.env.NODE_ENV);
--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,2 @@
 // keys.js - figure out what set of credentials to return
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./prod');
-} else {
-  module.exports = require('./dev');
-}
+module.exports = process.env.NODE_ENV === 'production' ? require('./prod') : require('./dev');
