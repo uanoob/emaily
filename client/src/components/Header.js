7aeb1e4107fbf35f82b1dfed5a3790bc6f770a5f
uanoob <usnet@ukr.net>
2018-02-19 11:13:11 +0200

    Add client part app

---
+++ client/src/components/Header.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Payments from './Payments';
+
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">
+              <button className="btn">Login With Google</button>
+            </a>
+          </li>
+        );
+      default:
+        return (
+          <div>
+            <li>
+              <Payments />
+            </li>
+            <li style={{ margin: '0 10px' }}>Credits: {this.props.auth.credits}</li>
+            <li>
+              <a href="/api/logout">Logout</a>
+            </li>
+          </div>
+        );
+    }
+  }
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link to={this.props.auth ? '/surveys' : '/'} className="left brand-logo">
+            Emaily
+          </Link>
+          <ul className="right">{this.renderContent()}</ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
