131b79bf914bc7a6f40fb6e9135418e5982e31e9
uanoob <usnet@ukr.net>
2018-02-21 13:39:15 +0200

    Add surveys on server

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,13 @@ import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
-
+import axios from 'axios';
 import App from './components/App';
 import reducers from './reducers';
+
+
+window.axios = axios;
+
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 const path = require('path');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -23,6 +24,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
